04fd6f9f3a77ce2e20964f11a8d7fa523ccecfc5
ivasitinha <maria.ivasita@escola.pr.gov.br>
2025-08-11 12:25:40 -0300

    Add files via upload

---
+++ terceiro-tri-main/ativ 2/main.js
@@ -0,0 +1,18 @@
+//cria referência ao from e aos elementos h3 e h4 (resposta)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+  const titulo = frm.inTitulo.value;             // obtem conteúdo dos campos
+  const duracao = Number(frm.inDuracao.value);
+
+  const horas = Math.floor(duracao / 60)       // arredonda para baixo resulta
+  const minutos = duracao % 60                   // obtém o resto da divisão
+
+  respl.innerText = titulo                     // exibe as respostas
+  resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`
+ 
+  e.preventDefault()                           // evita envio do form
+})
